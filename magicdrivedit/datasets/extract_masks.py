2ba27257b344048b9d6b42571e47c1e23d389ed1
Kaiwen Wang <kaiwen.wang@kit.edu>
2025-09-28 15:00:21 +0200

    update extract_masks for sweeps as well

--- magicdrivedit/datasets/extract_masks.py
+++ magicdrivedit/datasets/extract_masks.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
     parser.add_argument('--config', help='Config file', type=str, default=None)
     parser.add_argument('--checkpoint', help='Checkpoint file', type=str, default=None)
     parser.add_argument('--device', default='cuda:0', help='Device used for inference')
-  
+
     args = parser.parse_args()
     if args.config is None:
         args.config = os.path.join(args.segformer_path, 'local_configs', 'segformer', 'B5', 'segformer.b5.1024x1024.city.160k.py')
@@ -75,30 +75,66 @@ if __name__ == "__main__":
     save_dir = args.save_root
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
+    # Samples
     for cam in tqdm(cams, f'Extracting Masks ...'):
         cam_dir = os.path.join(args.data_root, "samples", cam)
         for img_file in os.listdir(cam_dir):
             img_path = os.path.join(cam_dir, img_file)
-        human_mask_dir = os.path.join(save_dir, "human", cam)
+        human_mask_dir = os.path.join(save_dir, "human/samples", cam)
         if not os.path.exists(human_mask_dir):
             os.makedirs(human_mask_dir)
-        vehicle_mask_dir = os.path.join(save_dir, "vehicle", cam)
+        vehicle_mask_dir = os.path.join(save_dir, "vehicle/samples", cam)
         if not os.path.exists(vehicle_mask_dir):
             os.makedirs(vehicle_mask_dir)
-        
+
         for filename in tqdm(os.listdir(cam_dir), f'Extracting Masks from {cam} ...'):
             human_mask_path = os.path.join(human_mask_dir, filename[:-4]+".png")
             vehicle_mask_path = os.path.join(vehicle_mask_dir, filename[:-4]+".png")
             if args.ignore_existing and os.path.exists(human_mask_path) and os.path.exists(vehicle_mask_path):
                 continue
             fpath = os.path.join(cam_dir, filename)
-            #---- Inference and save outputs
+            # ---- Inference and save outputs
             result = inference_segmentor(model, fpath)
             mask = result[0].astype(np.uint8)   # NOTE: in the settings of "cityscapes", there are 19 classes at most.
             # save human masks
             human_mask = np.isin(mask, dataset_classes_in_sematic['human'])
             imageio.imwrite(human_mask_path, human_mask.astype(np.uint8)*255)
 
-            # save vehicle 
+            # save vehicle
             vehicle_mask = np.isin(mask, dataset_classes_in_sematic['Vehicle'])
             imageio.imwrite(vehicle_mask_path, vehicle_mask.astype(np.uint8)*255)
+
+    # Sweeps
+    for cam in tqdm(cams, f"Extracting Masks ..."):
+        cam_dir = os.path.join(args.data_root, "sweeps", cam)
+        for img_file in os.listdir(cam_dir):
+            img_path = os.path.join(cam_dir, img_file)
+        human_mask_dir = os.path.join(save_dir, "human/sweeps", cam)
+        if not os.path.exists(human_mask_dir):
+            os.makedirs(human_mask_dir)
+        vehicle_mask_dir = os.path.join(save_dir, "vehicle/sweeps", cam)
+        if not os.path.exists(vehicle_mask_dir):
+            os.makedirs(vehicle_mask_dir)
+
+        for filename in tqdm(os.listdir(cam_dir), f"Extracting Masks from {cam} ..."):
+            human_mask_path = os.path.join(human_mask_dir, filename[:-4] + ".png")
+            vehicle_mask_path = os.path.join(vehicle_mask_dir, filename[:-4] + ".png")
+            if (
+                args.ignore_existing
+                and os.path.exists(human_mask_path)
+                and os.path.exists(vehicle_mask_path)
+            ):
+                continue
+            fpath = os.path.join(cam_dir, filename)
+            # ---- Inference and save outputs
+            result = inference_segmentor(model, fpath)
+            mask = result[0].astype(
+                np.uint8
+            )  # NOTE: in the settings of "cityscapes", there are 19 classes at most.
+            # save human masks
+            human_mask = np.isin(mask, dataset_classes_in_sematic["human"])
+            imageio.imwrite(human_mask_path, human_mask.astype(np.uint8) * 255)
+
+            # save vehicle
+            vehicle_mask = np.isin(mask, dataset_classes_in_sematic["Vehicle"])
+            imageio.imwrite(vehicle_mask_path, vehicle_mask.astype(np.uint8) * 255)
